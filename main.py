2cfc37558e0197d5215904bee261054dde27d8f2
Glenn-cloud <manakaglen10@gmail.com>
2025-09-17 22:59:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from statsmodels.tsa.arima.model import ARIMA as arima
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 
